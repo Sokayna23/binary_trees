4bb486f60609aad641a02363670d0cda7aeb2bb1
Sokayna <sokayna.bennasseur@gmail.com>
2023-08-29 00:43:05 -0400

    Task2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,28 @@
+#include "binary_tree.h"
+
+/**
+ * binary_tree_insert_right - inserts a node
+ *			as the right-child of another node.
+ * @parent: a pointer to the node to insert the righ-child in.
+ * @value: the value to store in the new node.
+ * Return: a pointer to the created node, otherwise NULL.
+ */
+binary_tree_t *binary_tree_insert_right(
+		binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+
+	if (!parent)
+		return (NULL);
+	node = binary_tree_node(parent, node);
+	if (!node)
+		return (NULL);
+	if (parent->right)
+	{
+		node->right = parent->right;
+		parent->right->parent = node;
+	}
+	parent->right = node;
+	return (node);
+}
